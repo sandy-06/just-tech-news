e695dbb8261b1bd1d66e07d9e6f6141af6c80816
Sandra Sanders <sandy-sanders@msn.com>
2022-01-17 16:01:17 -0700

    14.4.8

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const sequelize = require('./config/connection');
 const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
